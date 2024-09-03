ec83849a95f9b5e745b3cc4e8026da94b784679f
Piotr Frontczak <piotrf2023@gmail.com>
2024-09-03 21:05:32 +0200

    updates

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,28 +1,38 @@
-const express = require('express');
-const Joi = require('joi');
-const { addContact } = require('../../models/contacts');
+const Contact = require('../../models/contact'); 
 
-const router = express.Router();
+const listContacts = async () => {
+  return await Contact.find();
+};
 
-const contactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().required(),
-  favorite: Joi.boolean(),
-});
+const getContactById = async (contactId) => {
+  return await Contact.findById(contactId);
+};
 
-router.post('/', async (req, res, next) => {
-  try {
-    const { error } = contactSchema.validate(req.body);
-    if (error) {
-      return res.status(400).json({ message: `missing required ${error.details[0].context.key} field` });
-    }
+const removeContact = async (contactId) => {
+  return await Contact.findByIdAndDelete(contactId);
+};
 
-    const newContact = await addContact(req.body);
-    res.status(201).json(newContact);
-  } catch (error) {
-    next(error);
-  }
-});
+const addContact = async (body) => {
+  return await Contact.create(body);
+};
 
-module.exports = router;
+const updateContact = async (contactId, body) => {
+  return await Contact.findByIdAndUpdate(contactId, body, { new: true });
+};
+
+const updateStatusContact = async (contactId, favorite) => {
+  return await Contact.findByIdAndUpdate(
+    contactId,
+    { favorite },
+    { new: true }
+  );
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+  updateStatusContact,
+};
