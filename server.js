c5877c6919a3ba62f17750859811b56fdad84cb1
Piotr Frontczak <piotrf2023@gmail.com>
2024-09-03 20:05:58 +0200

    updates

--- server.js
+++ server.js
@@ -1,22 +1,8 @@
 const app = require('./app');
-require('./server');
+const connectDB = require('./config/db');
+
+connectDB();
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000");
 });
-
-const mongoose = require('mongoose');
-
-const DB_HOST = 'mongodb+srv://piotrf2023:milenka12@cluster0.vxeuh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; 
-
-mongoose.connect(DB_HOST, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => {
-    console.log("Database connection successful");
-  })
-  .catch(error => {
-    console.error("Database connection error:", error.message);
-    process.exit(1);
-  });
