cda7fca5843a567d838135cba3734747fc6a96b9
Piotr Frontczak <piotrf2023@gmail.com>
2024-09-03 21:52:27 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,61 +1,7 @@
 const express = require('express');
-const { listContacts, getContactById, addContact, removeContact, updateContact, updateStatusContact } = require('../../models/contact');
+const Contact = require('../../models/contact');
 
 const router = express.Router();
-
-router.get('/', async (req, res, next) => {
-  try {
-    const contacts = await listContacts();
-    res.json(contacts);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.get('/:contactId', async (req, res, next) => {
-  try {
-    const contact = await getContactById(req.params.contactId);
-    if (!contact) {
-      return res.status(404).json({ message: 'Not found' });
-    }
-    res.json(contact);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.post('/', async (req, res, next) => {
-  try {
-    const newContact = await addContact(req.body);
-    res.status(201).json(newContact);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.delete('/:contactId', async (req, res, next) => {
-  try {
-    const contact = await removeContact(req.params.contactId);
-    if (!contact) {
-      return res.status(404).json({ message: 'Not found' });
-    }
-    res.json({ message: 'contact deleted' });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put('/:contactId', async (req, res, next) => {
-  try {
-    const updatedContact = await updateContact(req.params.contactId, req.body);
-    if (!updatedContact) {
-      return res.status(404).json({ message: 'Not found' });
-    }
-    res.json(updatedContact);
-  } catch (error) {
-    next(error);
-  }
-});
 
 router.patch('/:contactId/favorite', async (req, res, next) => {
   try {
@@ -63,14 +9,21 @@ router.patch('/:contactId/favorite', async (req, res, next) => {
     if (favorite === undefined) {
       return res.status(400).json({ message: 'missing field favorite' });
     }
-    const updatedContact = await updateStatusContact(req.params.contactId, favorite);
+    
+    const updatedContact = await Contact.findByIdAndUpdate(
+      req.params.contactId, 
+      { favorite }, 
+      { new: true }
+    );
+    
     if (!updatedContact) {
       return res.status(404).json({ message: 'Not found' });
     }
+    
     res.json(updatedContact);
   } catch (error) {
     next(error);
   }
 });
 
-module.exports = router; 
+module.exports = router;
