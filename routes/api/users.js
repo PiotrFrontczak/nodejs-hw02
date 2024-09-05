43fcbe2b2dead9f43c640c348a997def9141d940
Piotr Frontczak <piotrf2023@gmail.com>
2024-09-05 20:23:50 +0200

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../../models/user');
-const auth = require('../../middlewares/auth');
+const auth = require('../../middleware/auth');
 const Joi = require('joi');
 
 const router = express.Router();
