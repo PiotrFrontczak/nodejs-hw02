ba57ba2578766459b6471b64766ec706f6647d08
Piotr Frontczak <piotrf2023@gmail.com>
2024-09-07 21:06:17 +0200

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,16 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../../models/user');
+const auth = require('../../middleware/auth');
 const multer = require('multer');
 const jimp = require('jimp');
 const path = require('path');
 const fs = require('fs').promises;
+const gravatar = require('gravatar');
+const Joi = require('joi');
+
+const router = express.Router();
 
 const upload = multer({ dest: 'tmp/' });
 
@@ -12,13 +21,13 @@ router.patch('/avatars', auth, upload.single('avatar'), async (req, res, next) =
     const newFilename = `${req.user._id}${ext}`;
 
     const avatar = await jimp.read(tempPath);
-    await avatar.resize(350, 350);
+    await avatar.resize(350, 350); 
     const avatarPath = path.join(__dirname, '../../public/avatars', newFilename);
-    await avatar.writeAsync(avatarPath); 
+    await avatar.writeAsync(avatarPath);
 
     await fs.unlink(tempPath);
 
-    const avatarURL = `/avatars/${newFilename}`;
+    const avatarURL = `/avatars/${newFilename}`; 
     req.user.avatarURL = avatarURL;
     await req.user.save();
 
