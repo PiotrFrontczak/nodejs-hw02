e487fccc6af5638148c4b80bd15512a566ceeac8
Piotr Frontczak <piotrf2023@gmail.com>
2024-09-07 20:49:29 +0200

    updates for hw05

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,13 +2,14 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../../models/user');
-const auth = require('../../middleware/auth');
+const auth = require('../../middlewares/auth');
 const Joi = require('joi');
+const gravatar = require('gravatar');
 
 const router = express.Router();
 
-const SECRET_KEY = process.env.JWT_SECRET || 'pvlOEEsUym3IUDRGZ05MwL5VvWxWcZQF';
-
+const SECRET_KEY = process.env.JWT_SECRET || 'your_jwt_secret_key';
+ 
 const signupSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
@@ -28,64 +29,22 @@ router.post('/signup', async (req, res, next) => {
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = await User.create({ email, password: hashedPassword });
+
+    const avatarURL = gravatar.url(email, { s: '250', r: 'pg', d: 'mm' });
+
+    const newUser = await User.create({
+      email,
+      password: hashedPassword,
+      avatarURL, 
+    });
 
     res.status(201).json({
       user: {
         email: newUser.email,
         subscription: newUser.subscription,
+        avatarURL: newUser.avatarURL,
       },
     });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.post('/login', async (req, res, next) => {
-  try {
-    const { error } = signupSchema.validate(req.body);
-    if (error) {
-      return res.status(400).json({ message: error.details[0].message });
-    }
-
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-      return res.status(401).json({ message: 'Email or password is wrong' });
-    }
-
-    const token = jwt.sign({ id: user._id }, SECRET_KEY, { expiresIn: '1h' });
-    user.token = token;
-    await user.save();
-
-    res.status(200).json({
-      token,
-      user: {
-        email: user.email,
-        subscription: user.subscription,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.get('/logout', auth, async (req, res, next) => {
-  try {
-    const user = req.user;
-    user.token = null;
-    await user.save();
-
-    res.status(204).send();
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.get('/current', auth, async (req, res, next) => {
-  try {
-    const { email, subscription } = req.user;
-    res.status(200).json({ email, subscription });
   } catch (error) {
     next(error);
   }
