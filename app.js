c79806398630c627af0b0e7c6e2a3b37adfc509b
Piotr Frontczak <piotrf2023@gmail.com>
2024-09-10 20:27:27 +0200

    updates

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 const path = require('path');
 const contactsRouter = require('./routes/api/contacts');
 const usersRouter = require('./routes/api/users');
+const emailRoutes = require('./routes/api/emails');
 const app = express();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
@@ -11,6 +13,7 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use('/api/emails', emailRoutes); 
 
 app.use('/avatars', express.static(path.join(__dirname, 'public', 'avatars')));
 app.use('/api/contacts', contactsRouter);
@@ -22,6 +25,12 @@ app.use((req, res) => {
 
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
+});
+
+const { sendEmail } = require('./email');
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
 });
 
 module.exports = app;
--- email.js
+++ email.js
@@ -1,25 +1,31 @@
-import nodemailer from "nodemailer";
+const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: "smtp.mailgun.org",
   port: 587,
-  secure: false, // true for port 465, false for other ports
+  secure: false,
   auth: {
     user: process.env.MAILGUN_USER,
     pass: process.env.MAILGUN_PASS,
   },
 });
 
-export async function sendEmail({ to, subject, text, html }) {
-    try {
-        const info = await transporter.sendMail({
-            from: '"Maddison Foo Koch 👻" <mycomp@gmail.com>', // sender address
-            to: "piotrf2023@gmail.com", // list of receivers
-            subject: "Hello ✔", // Subject line
-            text: "Hello world?", // plain text body
-            html: "<b>Hello world?</b>", // html body
-        });
+async function sendEmail({ to, subject, text, html }) {
+  try {
+    const info = await transporter.sendMail({
+      from: '"Maddison Foo Koch 👻" <mycomp@gmail.com>', 
+      to, 
+      subject, 
+      text, 
+      html, 
+    });
 
-        console.log("Message sent: %s", info.messageId);
-        // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
-    }}
+    console.log("Message sent: %s", info.messageId);
+    return info; 
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw error;
+  }
+}
+    
+module.exports = {sendEmail,};
